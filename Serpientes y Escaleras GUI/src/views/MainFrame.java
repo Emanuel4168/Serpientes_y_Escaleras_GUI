2924568e7ff26d090cc7e90510e3f1191781e601
Emanuel4168 <emanuel.l4168@gmail.com>
2019-02-25 18:47:33 -0700

    Creación de la clase principal y la clase Rutinas

---
+++ Serpientes y Escaleras GUI/src/views/MainFrame.java
@@ -0,0 +1,8 @@
+package views;
+
+public class MainFrame {
+	
+	private static final String [] IMAGE_NAMES = {"caballo.jpg","camaleon.jpg","delfin.jpg","elefante.jpg","gato.jpg","leon.jpg",
+			"murcielago.jpg","perro.jpg","pulpo.jpg","serpiente.jpg"};
+
+}
